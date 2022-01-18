36ec35dc980613d699bdb7269aa1ee41a9bd5e50
Xiaoqing He <sssxiaoqing@gmail.com>
2022-01-18 13:25:59 +0900

    add noise in raymarching

--- DirectX11/RaymarchingPS.hlsl
+++ DirectX11/RaymarchingPS.hlsl
@@ -19,58 +19,6 @@
 //SamplerState splr : register(s0);
 //Texture2D tex : register(t0);
 
-//static const float4 cloudColor = float4(1.0f, 1.0f, 1.0f, 1.0f);
-//static const int  loop = 464;
-//static const float size = 10.0f;
-//static const float noiseScale = 1.0f;
-//static const float radius = 1.0f;
-//static const float absorption = 50.0f;
-//static const float opacity = 100.0f;
-
-//static const float absorptionLight = 14.8f;
-//static const float opacityLight = 84.0f;
-//static const int loopLight = 6;
-//static const float lightStepScale = 0.5f;
-//static const float4 lightColor0 = float4(1.0f, 1.0f, 1.0f, 1.0f);
-
-//float hash(float n)
-//{
-//    return frac(sin(n) * 43758.5453f);
-//}
-
-//float noise(float3 x)
-//{
-//    float3 p = floor(x);
-//    float3 f = frac(x);
-//    f = f * f * (3.0f - 2.0f * f);
-    
-//    float n = p.x + p.y * 57.0f + p.z * 113.0f;
-//    float res = lerp(lerp(lerp(hash(n +   0.0f), hash(n +   1.0f), f.x), 
-//                          lerp(hash(n +  57.0f), hash(n +  58.0f), f.x), f.y),
-//                     lerp(lerp(hash(n + 113.0f), hash(n + 114.0f), f.x), 
-//                          lerp(hash(n + 170.0f), hash(n + 141.0f), f.x), f.y),f.z);
-    
-//    return res;
-//}
-
-//float fbm(float3 p)
-//{
-//    float3x3 m = float3x3(0.0f, 0.8f, 0.6f, -0.8f, 0.36f, -0.48f, -0.6f, -0.48f, 0.64f);    
-//    float f = 0.0f;
-//    f += 0.5f * noise(p);
-//    p = mul(m, p) * 2.02f;
-//    f += 0.3f * noise(p);
-//    p = mul(m, p) * 2.03f;
-//    f += 0.2f * noise(p);
-    
-//    return f;
-//}
-
-//float densityFunction(float3 p)
-//{
-//    return fbm(p * noiseScale) * size - length(p / radius);
-//}
-
 //float4 main(float3 worldPos : Position) : SV_Target
 //{    
 //    float step = 1.0f / loop;
@@ -162,26 +110,69 @@ cbuffer ObjectCBuf : register(b2)
     matrix worldInverse;
 };
 
-static const float4 color = float4(1.0f, 1.0f, 1.0f, 1.0f);
-static const int loop = 32;
-static const float intensity = 0.1f;
-static const float value = 0.5f;
+static const float4 cloudColor = float4(1.0f, 1.0f, 1.0f, 1.0f);
+static const int loop = 64;
+static const float intensity = 0.2f;
+static const float noiseScale = 3.0f;
+static const float radius = 1.0f;
+//static const float absorption = 50.0f;
+//static const float opacity = 100.0f;
+
+//static const float absorptionLight = 14.8f;
+//static const float opacityLight = 84.0f;
+//static const int loopLight = 6;
+//static const float lightStepScale = 0.5f;
+//static const float4 lightColor0 = float4(1.0f, 1.0f, 1.0f, 1.0f);
+
+float hash(float n)
+{
+    return frac(sin(n) * 43758.5453f);
+}
+
+float noise(float3 x)
+{
+    float3 p = floor(x);
+    float3 f = frac(x);
+    f = f * f * (3.0f - 2.0f * f);
+    
+    float n = p.x + p.y * 57.0f + p.z * 113.0f;
+    float res = lerp(lerp(lerp(hash(n + 0.0f), hash(n + 1.0f), f.x),
+                          lerp(hash(n + 57.0f), hash(n + 58.0f), f.x), f.y),
+                     lerp(lerp(hash(n + 113.0f), hash(n + 114.0f), f.x),
+                          lerp(hash(n + 170.0f), hash(n + 141.0f), f.x), f.y), f.z);
+    
+    return res;
+}
+
+float fbm(float3 p)
+{
+    float3x3 m = float3x3(0.0f, 0.8f, 0.6f, -0.8f, 0.36f, -0.48f, -0.6f, -0.48f, 0.64f);
+    float f = 0.0f;
+    f += 0.5f * noise(p);
+    p = mul(m, p) * 2.02f;
+    f += 0.3f * noise(p);
+    p = mul(m, p) * 2.03f;
+    f += 0.2f * noise(p);
+    
+    return f;
+}
 
 float densityFunction(float3 p)
 {
-    return value - length(p);
+    return fbm(p * noiseScale) - length(p / radius);
 }
 
 float4 main(float3 worldPos : Position) : SV_Target
 {
+    float step = 1.0f / loop;
+
     float3 worldDir = normalize(worldPos - cameraPos);
     
     float3 localPos = (float3) mul(worldInverse, float4(worldPos, 1.0f));
-    //float3 localDir = (float3) mul(worldInverse, float4(worldDir, 1.0f));
     float3 localDir = normalize(mul((float3x3) worldInverse, worldDir));
-
-    float step = 1.0f / loop;
     float3 localStep = localDir * step;
+    float jitter = hash(localPos.x + localPos.y * 10.0f + localPos.z * 100.0f);
+    localPos += jitter * localStep;
     
     float alpha = 0.0f;
     
@@ -196,13 +187,13 @@ float4 main(float3 worldPos : Position) : SV_Target
         
         localPos += localStep;
         
-        if (!all(max(value - abs(localPos), 0.0f)))
+        if (!all(max(0.5f - abs(localPos), 0.0f)))
         {
             break;
         }
     }
     
-    float4 colorOut = color;
+    float4 colorOut = cloudColor;
     colorOut.a *= alpha;
     return colorOut;
 }
