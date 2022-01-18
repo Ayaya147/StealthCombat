c2e2e40e1daf467ca80093239275bd127750ccd7
Xiaoqing He <sssxiaoqing@gmail.com>
2022-01-18 13:41:07 +0900

    add absorption and opactity in raymartching

--- DirectX11/RaymarchingPS.hlsl
+++ DirectX11/RaymarchingPS.hlsl
@@ -1,97 +1,3 @@
-//cbuffer CBuf : register(b0)
-//{
-//    float3 cameraPos;
-//    float3 ambientLight;
-//};
-
-//cbuffer DirectLightCBuf : register(b1)
-//{
-//    float3 direction;
-//    float diffuseColor;
-//    float specColor;
-//};
-
-//cbuffer ObjectCBuf : register(b2)
-//{
-//    matrix worldInverse;
-//};
-
-//SamplerState splr : register(s0);
-//Texture2D tex : register(t0);
-
-//float4 main(float3 worldPos : Position) : SV_Target
-//{    
-//    float step = 1.0f / loop;
-
-//    float3 worldDir = normalize(worldPos - cameraPos);
-    
-//    float3 localPos = (float3) mul(worldInverse, float4(worldPos, 1.0f));
-//    float3 localDir = (float3) mul(worldInverse, float4(worldDir, 1.0f));
-//    //float3 localDir = normalize(mul((float3x3) worldInverse, worldDir));
-
-//    float3 localStep = localDir * step;
-//    float jitter = hash(localPos.x + localPos.y * 10.0f + localPos.z * 100.0f);
-//    localPos += jitter * localStep;
-    
-//    //float lightStep = 1.0f / loopLight;
-//    //float3 localLightDir = (float3) mul(worldInverse, float4(direction, 1.0f));
-//    //float3 localLightStep = localLightDir * lightStep * lightStepScale;
-    
-//    float4 color = float4(cloudColor.rgb, 0.0f);
-//    float transmittance = 1.0f;    
-    
-//    for (int i = 0; i < loop; i++)
-//    {
-//        float density = densityFunction(localPos);
-        
-//        if (density > 0.0f)
-//        {
-//            float d = density * step;
-//            transmittance *= 1.0f - d * absorption;
-            
-//            if (transmittance < 0.01f)
-//            {
-//                break;
-//            }
-            
-//            //float transmittanceLight = 1.0f;
-//            //float3 lightPos = localPos;
-            
-//            //for (int j = 0; j < loopLight; j++)
-//            //{
-//            //    float densityLight = densityFunction(lightPos);
-                
-//            //    if (densityLight>0.0f)
-//            //    {
-//            //        float dl = densityLight * lightStep;
-//            //        transmittanceLight *= 1.0f - dl * absorptionLight;
-                    
-//            //        if(transmittanceLight<0.01f)
-//            //        {
-//            //            transmittanceLight = 0.0f;
-//            //            break;
-//            //        }
-//            //    }
-                
-//            //    lightPos += localLightStep;
-//            //}
-            
-//            color.a += opacity * d * transmittance;
-//            //color.rgb += (lightColor0 * (opacityLight * d * transmittance * transmittanceLight)).rgb;
-//        }
-        
-//        color = clamp(color, 0.0f, 1.0f);
-//        localPos += localStep;
-        
-//        if (!all(max(size - abs(localPos), 0.0f)))
-//        {
-//            break;
-//        }
-//    }
-    
-//    return color;
-//}
-
 cbuffer CBuf : register(b0)
 {
     float3 cameraPos;
@@ -111,12 +17,11 @@ cbuffer ObjectCBuf : register(b2)
 };
 
 static const float4 cloudColor = float4(1.0f, 1.0f, 1.0f, 1.0f);
-static const int loop = 64;
-static const float intensity = 0.2f;
-static const float noiseScale = 3.0f;
-static const float radius = 1.0f;
-//static const float absorption = 50.0f;
-//static const float opacity = 100.0f;
+static const int loop = 32;
+static const float noiseScale = 5.0f;
+static const float radius = 0.8f;
+static const float absorption = 50.0f;
+static const float opacity = 100.0f;
 
 //static const float absorptionLight = 14.8f;
 //static const float opacityLight = 84.0f;
@@ -136,8 +41,8 @@ float noise(float3 x)
     f = f * f * (3.0f - 2.0f * f);
     
     float n = p.x + p.y * 57.0f + p.z * 113.0f;
-    float res = lerp(lerp(lerp(hash(n + 0.0f), hash(n + 1.0f), f.x),
-                          lerp(hash(n + 57.0f), hash(n + 58.0f), f.x), f.y),
+    float res = lerp(lerp(lerp(hash(n +   0.0f), hash(n +   1.0f), f.x),
+                          lerp(hash(n +  57.0f), hash(n +  58.0f), f.x), f.y),
                      lerp(lerp(hash(n + 113.0f), hash(n + 114.0f), f.x),
                           lerp(hash(n + 170.0f), hash(n + 141.0f), f.x), f.y), f.z);
     
@@ -146,12 +51,15 @@ float noise(float3 x)
 
 float fbm(float3 p)
 {
-    float3x3 m = float3x3(0.0f, 0.8f, 0.6f, -0.8f, 0.36f, -0.48f, -0.6f, -0.48f, 0.64f);
+    float3x3 m = float3x3(
+         0.00f,  0.80f,  0.60f,
+        -0.80f,  0.36f, -0.48f,
+        -0.60f, -0.48f,  0.64f
+    );
+    
     float f = 0.0f;
-    f += 0.5f * noise(p);
-    p = mul(m, p) * 2.02f;
-    f += 0.3f * noise(p);
-    p = mul(m, p) * 2.03f;
+    f += 0.5f * noise(p); p = mul(m, p) * 2.02f;
+    f += 0.3f * noise(p); p = mul(m, p) * 2.03f;
     f += 0.2f * noise(p);
     
     return f;
@@ -174,17 +82,27 @@ float4 main(float3 worldPos : Position) : SV_Target
     float jitter = hash(localPos.x + localPos.y * 10.0f + localPos.z * 100.0f);
     localPos += jitter * localStep;
     
-    float alpha = 0.0f;
+    float4 color = float4(cloudColor.rgb, 0.0f);
+    float transmittance = 1.0f;
     
     for (int i = 0; i < loop; i++)
     {
         float density = densityFunction(localPos);
         
-        if (density > 0.001f)
+        if (density > 0.0f)
         {
-            alpha += (1.0f - alpha) * density * intensity;
+            float d = density * step;
+            transmittance *= 1.0f - d * absorption;
+            
+            if (transmittance < 0.01f)
+            {
+                break;
+            }
+            
+            color.a += opacity * d * transmittance;
         }
         
+        color = clamp(color, 0.0f, 1.0f);
         localPos += localStep;
         
         if (!all(max(0.5f - abs(localPos), 0.0f)))
@@ -192,8 +110,6 @@ float4 main(float3 worldPos : Position) : SV_Target
             break;
         }
     }
-    
-    float4 colorOut = cloudColor;
-    colorOut.a *= alpha;
-    return colorOut;
+
+    return color;
 }
