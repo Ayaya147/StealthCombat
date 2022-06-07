837ab8f1fe1dcf2dab3bc1232de7bcd0686a9b8c
Xiaoqing He <sssxiaoqing@gmail.com>
2022-06-07 12:27:51 +0900

    add frame in screenshot texture

--- DirectX11/LaplacianFilterPS.hlsl
+++ DirectX11/LaplacianFilterPS.hlsl
@@ -1,25 +1,43 @@
 SamplerState splr : register(s0);
 Texture2D tex : register(t0);
 
+static const float weight[] =
+{
+    1.0f, 1.0f, 1.0f,
+    1.0f, -8.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,
+};
+
 float4 main(float2 tc : TEXCOORD, float4 pos : SV_POSITION) : SV_TARGET
 {
     float w = 0.0f;
-    float h = 0.0f;    
+    float h = 0.0f;
     tex.GetDimensions(w, h);
     
     float invW = 1.0f / w;
     float invH = 1.0f / h;
     
-    float4 c0 = tex.Sample(splr, tc + float2(invW *  0, invH *  0));
-    float4 c1 = tex.Sample(splr, tc + float2(invW *  0, invH * -1));
-    float4 c2 = tex.Sample(splr, tc + float2(invW *  0, invH *  1));
-    float4 c3 = tex.Sample(splr, tc + float2(invW * -1, invH *  0));
-    float4 c4 = tex.Sample(splr, tc + float2(invW *  1, invH *  0));
-    float4 c5 = tex.Sample(splr, tc + float2(invW * -1, invH * -1));
-    float4 c6 = tex.Sample(splr, tc + float2(invW *  1, invH * -1));
-    float4 c7 = tex.Sample(splr, tc + float2(invW * -1, invH *  1));
-    float4 c8 = tex.Sample(splr, tc + float2(invW *  1, invH *  1));
+    float4 c[9];
+    for (int j = -1; j <= 1; j++)
+    {
+        for (int i = -1; i <= 1; i++)
+        {
+            c[(j + 1) * 3 + i + 1] = tex.Sample(splr, tc + float2(invW * i, invH * j));
+        }
+    }
 
-    float4 color = c8 + c7 + c6 + c5 + c4 + c3 + c2 + c1 + (c0 * -8.0);
-    return float4(color.rgb, 1.0f);
+    float4 color = (float4) 0.0f;
+    for (int i = 0; i < 9; i++)
+    {
+        color += c[i] * weight[i];
+    }
+    color.a = 1.0f;
+    
+    float range = 0.01;
+    if (tc.x <= range || tc.x >= 1.0f - range || tc.y <= range || tc.y >= 1.0f - range)
+    {
+        color = float4(1.0f, 1.0f, 1.0f, 0.5f);
+    }
+    
+    return color;
 }
--- DirectX11/NegativeFilterPS.hlsl
+++ DirectX11/NegativeFilterPS.hlsl
@@ -3,7 +3,14 @@ Texture2D tex : register(t0);
 
 float4 main(float2 tc : TEXCOORD) : SV_TARGET
 {
-    float3 color = tex.Sample(splr, tc).rgb;
+    float4 color = tex.Sample(splr, tc);
+    color.a = 0.0f;
     
-    return float4((float3) 1.0f - color, 1.0f);
+    float range = 0.01;
+    if (tc.x <= range || tc.x >= 1.0f - range || tc.y <= range || tc.y >= 1.0f - range)
+    {
+        color = float4(1.0f, 1.0f, 1.0f, 0.5f);
+    }
+    
+    return (float4) 1.0f - color;
 }
